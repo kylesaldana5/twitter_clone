d479c9b864ce04c292554a6632cfea42b87441b4
Jake Scott <bemybanker@gmail.com>
2020-09-28 23:12:53 -0500

    nav bar in there but its bad

--- App/Containers/Root/RootScreen.js
+++ App/Containers/Root/RootScreen.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import React, { useEffect } from 'react'
 import NavigationService from 'App/Services/NavigationService'
 import AppNavigator from 'App/Navigators/AppNavigator'
@@ -5,11 +6,13 @@ import { View } from 'react-native'
 import StartupActions from 'App/Stores/Startup/Actions'
 import { Helpers } from 'App/Theme'
 import { useDispatch } from 'react-redux'
+import BottomNavBar from '../../Components/nav/bottomNavBar';
+
 
 const RootScreen = () => {
   // Deep linking prefix
   const prefix = 'boilerplate://';
-  
+
   const dispatch = useDispatch()
 
   useEffect(() => {
@@ -17,15 +20,16 @@ const RootScreen = () => {
   }, [])
 
   return (
-    <View style={Helpers.fill}>
-      <AppNavigator
-        // Initialize the NavigationService (see https://reactnavigation.org/docs/en/navigating-without-navigation-prop.html)
-        ref={(navigatorRef) => {
-          NavigationService.setTopLevelNavigator(navigatorRef)
-        }}
-        uriPrefix={prefix}
-      />
-    </View>
+      <View style={Helpers.fill}>
+        <AppNavigator
+          // Initialize the NavigationService (see https://reactnavigation.org/docs/en/navigating-without-navigation-prop.html)
+          ref={(navigatorRef) => {
+            NavigationService.setTopLevelNavigator(navigatorRef)
+          }}
+          uriPrefix={prefix}
+        />
+        <BottomNavBar />
+      </View>
   )
 }
 
